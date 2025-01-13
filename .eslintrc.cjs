4333cbd2c5fc681915b336b7f8650268d19885c6
Sam Davidoff <sam@mfbtech.com>
2025-01-13 01:08:09 +0000

    update eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,24 +7,28 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module"
   },
-  files: ["*.ts", "*.tsx"],
-  rules: {
-    "@typescript-eslint/consistent-type-definitions": [1, "type"],
-    "@typescript-eslint/naming-convention": [
-      "error",
-      {
-        selector: "parameter",
-        format: ["camelCase"],
-        leadingUnderscore: "allow"
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/consistent-type-definitions": [1, "type"],
+        "@typescript-eslint/naming-convention": [
+          "error",
+          {
+            selector: "parameter",
+            format: ["camelCase"],
+            leadingUnderscore: "allow"
+          }
+        ],
+        // allow use of null
+        "@rushstack/no-new-null": 0,
+        // disable requiring explicit types in locations where inference is possible
+        "@typescript-eslint/typedef": 0,
+        "@rushstack/typedef-var": 0,
+        // disable required jest hoisting to allow more flexibility
+        "@rushstack/hoist-jest-mock": 0,
+        "@typescript-eslint/method-signature-style": ["warn", "property"]
       }
-    ],
-    // allow use of null
-    "@rushstack/no-new-null": 0,
-    // disable requiring explicit types in locations where inference is possible
-    "@typescript-eslint/typedef": 0,
-    "@rushstack/typedef-var": 0,
-    // disable required jest hoisting to allow more flexibility
-    "@rushstack/hoist-jest-mock": 0,
-    "@typescript-eslint/method-signature-style": ["warn", "property"]
-  }
+    }
+  ]
 }
