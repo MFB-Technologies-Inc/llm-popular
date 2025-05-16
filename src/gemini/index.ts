3eeea4f94cea48ed8656a7e00d95c9356984246e
Sam Davidoff <sam@mfbtech.com>
2025-05-16 20:30:25 +0000

    update models and add option to call from browser code

--- src/gemini/index.ts
+++ src/gemini/index.ts
@@ -6,10 +6,9 @@ import { ModelApi, ChatPrompt } from "@mfbtech/llm-api-types"
 import { nanoid } from "nanoid"
 
 type GeminiModel =
-  | "gemini-2.5-pro-preview-03-25"
+  | "gemini-2.5-pro-preview-05-06"
+  | "gemini-2.5-flash-preview-04-17"
   | "gemini-2.0-flash"
-  | "gemini-1.5-pro"
-  | "gemini-1.5-flash"
 
 export function buildGeminiLlm(
   model: GeminiModel,
